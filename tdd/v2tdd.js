929dce591f3f06e9958a5ffd7e5748b2e36f7207
pmontgomery <finch.wren.swift@gmail.com>
2022-09-24 14:03:54 -0700

    second pass

---
+++ tdd/v2tdd.js
@@ -0,0 +1,145 @@
+
+
+
+const {
+    yellow, green, verdict, cyan, closeEnough
+} = require("../utils/utils.js")
+
+const { read_csv_file, getMatrix, getPredictions, train, beginTraining, get_sum_of_labels } = require("../training/version2.js")
+
+
+const get_data_via_file_test = async () => {
+    const dependentVariables = ["Outlook", "Temperature", "Humidity", "Windy"]
+    const prediction = "Play"
+    const actual = await read_csv_file("./../data/weather.csv", dependentVariables, prediction)
+    const expected = { "predictions": { "No": 5, "Yes": 9 }, "matrix": { "Outlook": { "No": { "Rainy": 3, "Sunny": 2 }, "Yes": { "Overcast": 4, "Sunny": 3, "Rainy": 2 } }, "Temperature": { "No": { "Hot": 2, "Cool": 1, "Mild": 2 }, "Yes": { "Hot": 2, "Mild": 4, "Cool": 3 } }, "Humidity": { "No": { "High": 4, "Normal": 1 }, "Yes": { "High": 3, "Normal": 6 } }, "Windy": { "No": { "False": 2, "True": 3 }, "Yes": { "False": 6, "True": 3 } } } }
+    verdict(actual, expected, "get_data_test: read csv file")
+    return actual
+}
+
+const set_data_explicitly_test = async (expected) => {
+    beginTraining()
+
+    train({ "Outlook": "Rainy", "Temperature": "Hot", "Humidity": "High", "Windy": "False" }, 'No')
+    train({ "Outlook": "Rainy", "Temperature": "Hot", "Humidity": "High", "Windy": "True" }, 'No')
+    train({ "Outlook": "Overcast", "Temperature": "Hot", "Humidity": "High", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Sunny", "Temperature": "Mild", "Humidity": "High", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Sunny", "Temperature": "Cool", "Humidity": "Normal", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Sunny", "Temperature": "Cool", "Humidity": "Normal", "Windy": "True" }, 'No')
+    train({ "Outlook": "Overcast", "Temperature": "Cool", "Humidity": "Normal", "Windy": "True" }, 'Yes')
+    train({ "Outlook": "Rainy", "Temperature": "Mild", "Humidity": "High", "Windy": "False" }, 'No')
+    train({ "Outlook": "Rainy", "Temperature": "Cool", "Humidity": "Normal", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Sunny", "Temperature": "Mild", "Humidity": "Normal", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Rainy", "Temperature": "Mild", "Humidity": "Normal", "Windy": "True" }, 'Yes')
+    train({ "Outlook": "Overcast", "Temperature": "Mild", "Humidity": "High", "Windy": "True" }, 'Yes')
+    train({ "Outlook": "Overcast", "Temperature": "Hot", "Humidity": "Normal", "Windy": "False" }, 'Yes')
+    train({ "Outlook": "Sunny", "Temperature": "Mild", "Humidity": "High", "Windy": "True" }, 'No')
+
+    const matrix = getMatrix()
+    const predictions = getPredictions()
+
+    const p = { "No": 5, "Yes": 9 }
+    const m = { "Outlook": { "No": { "Rainy": 3, "Sunny": 2 }, "Yes": { "Overcast": 4, "Sunny": 3, "Rainy": 2 } }, "Temperature": { "No": { "Hot": 2, "Cool": 1, "Mild": 2 }, "Yes": { "Hot": 2, "Mild": 4, "Cool": 3 } }, "Humidity": { "No": { "High": 4, "Normal": 1 }, "Yes": { "High": 3, "Normal": 6 } }, "Windy": { "No": { "False": 2, "True": 3 }, "Yes": { "False": 6, "True": 3 } } }
+
+    verdict(matrix, m, "set_data_explicitly_test: matrix")
+    verdict(predictions, p, "set_data_explicitly_test: predictions")
+
+    return { predictions, matrix }
+}
+const test_get_probabilityA_programmatically = () => {
+    const prediction = "Yes"
+    const seek = {
+        Outlook: "Sunny",
+        Temperature: "Hot",
+        Humidity: "Normal",
+        Windy: "False"
+    }
+    const A = get_sum_of_labels(seek, prediction)
+    const isOk = closeEnough(A, 0.001, 0.021164021164021163)
+    verdict(isOk, true, "test_get_probabilityA_programmatically: " + A)
+}
+
+const test_get_the_answer = () => {
+    const prediction = "Yes"
+    const seek = {
+        Outlook: "Sunny",
+        Temperature: "Hot",
+        Humidity: "Normal",
+        Windy: "False"
+    }
+    const A = get_sum_of_labels(seek, prediction)
+    const isOk = closeEnough(A, 0.001, 0.021164021164021163)
+    // green("A=" + A )
+
+    const B = get_sum_of_labels(seek, "No")
+    // green("B=" + B )
+
+
+    const all = A + B 
+    const yep = A / all 
+    const nah = B / all 
+
+    const actual = {
+        yep, 
+        nah
+    }
+    const expected = { yep: 0.8223684210526315, nah: 0.17763157894736847 }
+    verdict(actual, expected, "test_get_the_answer: " + JSON.stringify(actual))
+}
+
+const test_get_probabilityA_manually = () => {
+    // today = (Sunny, Hot, Normal, False)
+    // Numbers from the 'weather.csv' data
+    const sunny = 3 / 9 // sunny and 'yes'
+    const hot = 2 / 9 // hot and 'yes'
+    const normalHumid = 6 / 9 // norm humid and 'yes'
+    const noWind = 6 / 9 // no wind and 'yes'
+    const yes = 9 / 14 // overall 'yes'
+
+    const total = sunny * hot * normalHumid * noWind * yes
+    const isOk = closeEnough(total, 0.001, 0.021164021164021163)
+    verdict(isOk, true, "test_get_probabilityA_manually: " + total)
+}
+
+const test_get_answer_manually = () => {
+    // today = (Sunny, Hot, Normal, False)
+    // Numbers from the 'weather.csv' data
+    const sunny = 3 / 9 // sunny and 'yes'
+    const hot = 2 / 9 // hot and 'yes'
+    const normalHumid = 6 / 9 // norm humid and 'yes'
+    const noWind = 6 / 9 // no wind and 'yes'
+    const yes = 9 / 14 // overall 'yes'
+
+    const total = sunny * hot * normalHumid * noWind * yes
+
+    const sunny2 = 2 / 5 // sunny and 'no'
+    const hot2 = 2 / 5 // hot and 'no'
+    const normalHumid2 = 1 / 5 // norm humid and 'no'
+    const noWind2 = 2 / 5 // no wind and 'no'
+    const yes2 = 5 / 14 // overall 'no'
+
+    const total2 = sunny2 * hot2 * normalHumid2 * noWind2 * yes2
+
+    const all = total + total2 
+    const yep = total / all 
+    const nah = total2 / all 
+
+    const actual = {
+        yep, 
+        nah
+    }
+
+    const expected = { yep: 0.8223684210526315, nah: 0.17763157894736847 }
+    verdict(actual, expected, "test_get_answer_manually: " + JSON.stringify(actual))
+}
+if (require.main === module) {
+    const main = async () => {
+        await get_data_via_file_test()
+        set_data_explicitly_test()
+        test_get_probabilityA_manually()
+        test_get_probabilityA_programmatically()
+        test_get_answer_manually()
+        test_get_the_answer()
+    }
+    main()
+}
---
+++ training/version2.js
@@ -0,0 +1,106 @@
+const csv = require('csv-parser')
+const {
+    yellow, green, cyan, red
+} = require("../utils/utils.js")
+const fs = require('fs')
+const { info } = require('console')
+
+let predictions = {}     
+let matrix = {}
+function getMatrix() {
+    return matrix
+}
+function getPredictions() { 
+    return predictions
+}
+function beginTraining() {
+    predictions = {}     
+    matrix = {}    
+}
+
+function get_sum_of_labels(labels, prediction) {
+    let probs = []
+    const p = predictions[prediction]
+    for ( let label in labels ) {
+        const value = labels[label]
+        const n = matrix[label][prediction][value]
+        //green(n + " / " + p )
+        probs.push(n / p)
+    }
+    let allThePredictions = 0 
+    for ( let k in predictions ) {
+        allThePredictions += predictions[k]
+    }
+    //green( p + " / " + allThePredictions)
+    probs.push(p / allThePredictions)
+
+    let result = probs[0] 
+    for ( let i = 1; i < probs.length; i++ ) { 
+        result *= probs[i] 
+    }
+    return result
+}
+
+
+
+
+
+function train( information, prediction) {
+    //cyan( "train(" + JSON.stringify(information) + ",'" + prediction + "')")
+    // dependents!
+    for ( let label in information ) { 
+        let value = information[label]        
+        if ( ! matrix.hasOwnProperty(label) ) {
+            matrix[label] = {}
+        }
+        if ( ! matrix[label].hasOwnProperty(prediction)) {
+            matrix[label][prediction] = {}
+        }
+        if ( ! matrix[label][prediction].hasOwnProperty(value)) {
+            matrix[label][prediction][value] = 1
+        } else {
+            matrix[label][prediction][value]++
+        }
+    } 
+    // predictions!
+    if ( predictions.hasOwnProperty(prediction)) {
+        predictions[prediction]++
+    } else {
+        predictions[prediction] = 1
+    }
+}
+async function read_csv_file(path_to_file, dependentLabels, prediction) {
+    predictions = {}     
+    matrix = {}
+    return new Promise((resolve, reject) => {
+        let rowIndex = 0 
+        fs.createReadStream(path_to_file)
+            .pipe(csv())
+            .on('data', (row) => {
+                    let information = {} 
+                    dependentLabels.forEach((label)=> {
+                        information[label] = row[label]
+                    })
+                    train( information, row[prediction])
+
+                rowIndex++
+            })
+            .on('end', () => {
+                resolve({ predictions, matrix} )
+            });
+    });
+}
+
+
+if (require.main === module) {
+    // Just a self test: Included here mostly to show how to use this.
+    const main = async (path_to_data, empty_matrix) => { 
+        const dependentVariables = ["Outlook", "Temperature", "Humidity", "Windy"]
+        const prediction = "Play"
+        const x = await read_csv_file("./../data/weather.csv", dependentVariables, prediction)
+        cyan(x)
+        // cyan("The end")
+    }
+    main()
+}
+module.exports = { read_csv_file, getMatrix,getPredictions,train, beginTraining, get_sum_of_labels  }
